acc2c402fb033b93168bb7b4b06cc8890bb54f1b
Jones <daj59@pitt.edu>
2019-03-02 15:00:38 -0500

    Completed Game - Refractoring - Modulization of Code

--- ruby_rush.rb
+++ ruby_rush.rb
@@ -1,11 +1,8 @@
-def msg
-  print 'Usage:
-  ruby ruby_rush.rb *seed* *num_prospectors* *num_turns*
-  *seed* should be an integer
-  *num_prospectors* should be a non-negative integer
-  *num_turns* should be a non-negative integer'
-  exit(1)
-end
+require_relative 'game.rb'
+require_relative 'locations.rb'
+require_relative 'msg.rb'
 
 args = ARGV.to_a.map(&:to_i)
 msg if args.length != 3 || !args.all?(&:integer?) || args[1] < 0 || args[2] < 0
+
+game_engine(Random.new(args[0]), args[1], args[2], init_locations)
