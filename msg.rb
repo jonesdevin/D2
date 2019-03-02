3a4238f26f06a062c2f21dc1504ea569447c983d
Jones <daj59@pitt.edu>
2019-03-02 15:03:59 -0500

    added files

---
+++ msg.rb
@@ -0,0 +1,8 @@
+def msg
+  print 'Usage:
+  ruby ruby_rush.rb *seed* *num_prospectors* *num_turns*
+  *seed* should be an integer
+  *num_prospectors* should be a non-negative integer
+  *num_turns* should be a non-negative integer'
+  exit(1)
+end
